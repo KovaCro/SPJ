70bf08327aecdeebe4c3aaa981543970c20d0210
Marin Kovač <marin.kovac04@gmail.com>
2024-07-04 11:54:31 +0200

    .

--- main.hs
+++ main.hs
@@ -17,7 +17,7 @@ shift d t = f 0 t
         f c (Lam t) = Lam $ f (c + 1) t
         f c (App t u) = App (f c t) (f c u)
 
--- Primjer (\.\.2 1 (\.3))  
+-- Primjer (λλ 2 1 (λ 3))  
 example_shift = shift 2 (Lam (Lam (App (App (Var 2) (Var 1)) (Lam (Var (3))))))
 
 -- Supstitucija [y -> s]t
@@ -27,14 +27,14 @@ subst j s t = f 0 t
         f c (Lam t) = Lam $ f (c + 1) t
         f c (App t u) = App (f c t) (f c u)
 
--- Primjer [1 -> \.0 1] (\.\.3 0)
+-- Primjer [1 -> λ0 1] (λλ 3 0)
 example_subst = subst 1 (Lam (App (Var 0) (Var 1))) (Lam (Lam (App (Var 3) (Var 0))))
 
 -- Beta redukcija
 beta :: Term -> Term -> Term
 beta s t = shift (-1) $ subst 0 (shift 1 s) t
 
--- Primjer (\.0) (\.1)
+-- Primjer (λ0) (λ1)
 example_beta = beta (Lam (Var 1)) (Var 0)
 
 -- Korak evaluacije
@@ -49,7 +49,7 @@ eval :: Term -> Term
 eval t = if t == t' then t else eval t'
   where t' = eval_step t
 
--- Primjer (\.(\.0 1) 0) (\.0)
+-- Primjer (λ(λ0 1) 0) (λ0)
 example_eval = eval (App (Lam (App (Lam (App (Var 0) (Var 1))) (Var 0))) (Lam (Var 0)))
 
 -- Obrisi Type iz Terma
@@ -58,7 +58,7 @@ eraseType (TVar x) = Var x
 eraseType (TLam t _) = Lam (eraseType t)
 eraseType (TApp t1 t2) = App (eraseType t1) (eraseType t2)
 
--- Primjer (\.:A(\.:B 0 1) 0) (\.:C 0)
+-- Primjer (λ:A(λ:B 0 1) 0) (λ:C 0)
 example_erase = eraseType (TApp (TLam (TApp (TLam (TApp (TVar 0) (TVar 1)) (T 1)) (TVar 0)) (T 0)) (TLam (TVar 0) (T 2)))
 
 -- Spaja Typeove
@@ -85,18 +85,22 @@ inferType (TApp t1 t2) ctx = do
     _ -> Nothing
 
 -- A = T 0, B = T 1
--- Primjer (\.:A\.:(A->B) 0)
+-- Primjer (λ:Aλ:(A->B) 0)
 example_infer1 = inferType (TLam (TLam (TVar 0) (Arrow (T 0) (T 1))) (T 0)) []
--- Primjer (\.:A\.:(A->B) 0 1)
+-- Primjer (λ:Aλ:(A->B) 0 1)
 example_infer2 = inferType (TLam (TLam (TApp (TVar 0) (TVar 1)) (Arrow (T 0) (T 1))) (T 0)) []
--- Primjer (\.:A\.:(A->B) 0 1) (x:A)
+-- Primjer (λ:Aλ:(A->B) 0 1) (x:A)
 example_infer3 = inferType (TApp (TLam (TLam (TApp (TVar 0) (TVar 1)) (Arrow (T 0) (T 1))) (T 0)) (TVar 10)) [(10, T 0)]
--- Primjer (\.:A\.:(A->B) 0 1) (x:B)
+-- Primjer (λ:Aλ:(A->B) 0 1) (x:B)
 example_infer4 = inferType (TApp (TLam (TLam (TApp (TVar 0) (TVar 1)) (Arrow (T 0) (T 1))) (T 0)) (TVar 10)) [(10, T 1)]
 
 -- Evaluacija Typed Lambde
 teval :: TypedTerm -> Context -> Term
 teval t c = if inferType t c == Nothing then Error else eval $ eraseType $ t
 
--- Primjer (\.:A\.:(A->B) 0 1) (x:A)
-teval_example = teval (TApp (TLam (TLam (TApp (TVar 0) (TVar 1)) (Arrow (T 0) (T 1))) (T 0)) (TVar 10)) [(10, T 0)]
+-- Primjer (λ:Aλ:(A->B) 0 1) (x:A)
+example_teval1 = teval (TApp (TLam (TLam (TApp (TVar 0) (TVar 1)) (Arrow (T 0) (T 1))) (T 0)) (TVar 10)) [(10, T 0)]
+-- Primjer (λ:Aλ:(A->B) 0 1) (x:B)
+example_teval2 = teval (TApp (TLam (TLam (TApp (TVar 0) (TVar 1)) (Arrow (T 0) (T 1))) (T 0)) (TVar 10)) [(10, T 1)]
+-- Primjer (λ:(A->A)λ:A 1 0)) (λ:A 0)
+example_teval3 = teval (TApp (TLam (TLam (TApp (TVar 1) (TVar 0)) (T 0)) (Arrow (T 0) (T 0))) (TLam (TVar 0) (T 0))) []
